28c7adf08451f1af1a6526383ea0cbe96e0171e1
Olio <jnugg0819@users.noreply.github.com>
2025-03-21 12:23:14 +0900

    삽입 정렬 간단구현

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,24 @@
 package com.olio.gym.fit
 
-import com.olio.gym.fit.syntax.Identifier
-
 
 fun main() {
+    val numbers = listOf(6,2,8,4,16,14)
+    println("Get Even Numbers ${getEvenNumbers(numbers)}")
+}
 
-    val identifier = Identifier()
-    identifier.getIdentifierCount("홍길동")
+fun getEvenNumbers(numbers: List<Int>): List<Int> {
+    val newNumbers = numbers.filter { it % 2 == 0 }.toMutableList()
+
+    for(i in 1 until newNumbers.size) {
+        val current = newNumbers[i]
+        var j = i - 1
+
+        while (j >= 0 && newNumbers[j] > current) {
+            newNumbers[j + 1] = newNumbers[j]
+            j--
+        }
+        newNumbers[j + 1] = current
+    }
+
+    return newNumbers
 }
