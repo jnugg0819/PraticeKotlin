1a4be48cd28ac5a293fa14e4857c06ad33dc8bb6
Olio <jnugg0819@users.noreply.github.com>
2025-03-24 17:09:22 +0900

    Anagram 원래 존재하던 맵에서 해당 스트링 빼주는 방식으로 변경

--- src/main/kotlin/algorithms/isAnagram.kt
+++ src/main/kotlin/algorithms/isAnagram.kt
@@ -3,19 +3,20 @@ package com.olio.gym.fit.algorithms
 
 fun isAnagram(a: String, b: String): Boolean {
 
-    if (a.length != b.length) return false
-
-    val mapOfAChar = mutableMapOf<Char, Int>()
-    val mapOfBChar = mutableMapOf<Char, Int>()
+    val mapOfChar = mutableMapOf<Char, Int>()
 
     for (char in a) {
-        mapOfAChar[char] = (mapOfAChar[char] ?: 0) + 1
+        mapOfChar[char] = (mapOfChar[char] ?: 0) + 1
     }
 
-    for(char in b) {
-        mapOfBChar[char] = (mapOfBChar[char] ?: 0) + 1
+    // 기존 맵에서 뺴주는 방식으로 변경.
+    for (char in b) {
+        val count = mapOfChar[char]
+        if (count == null || count == 0) {
+            return false
+        }
+        mapOfChar[char] = count - 1
     }
 
-    // 시간 복잡도 O(N) 각 키를 조회하면서 키값을 비교해준다. 선형적으로 증가함.
-    return mapOfAChar == mapOfBChar
+    return mapOfChar.all { it.value == 0 }
 }
