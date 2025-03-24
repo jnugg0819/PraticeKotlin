cbe38b1a08993b633d321d68d3877ec2e09cec20
Olio <jnugg0819@users.noreply.github.com>
2025-03-24 16:56:47 +0900

    Anagram 끌리는데로 다시 짬
    - 시간 복잡도 O(n^2)수정

--- src/main/kotlin/algorithms/isAnagram.kt
+++ src/main/kotlin/algorithms/isAnagram.kt
@@ -1,27 +1,21 @@
 package com.olio.gym.fit.algorithms
 
+
 fun isAnagram(a: String, b: String): Boolean {
 
     if (a.length != b.length) return false
 
-    var i = 0
-    var j = 0
-    val duplicateSet = mutableSetOf<Int>()
+    val mapOfAChar = mutableMapOf<Char, Int>()
+    val mapOfBChar = mutableMapOf<Char, Int>()
 
-    while (j <= b.length - 1 && i <= a.length - 1) {// 마지막을 넘어갔다는건 더이상 찾을게 없다는뜻.
-        if (a[i] == b[j] && !duplicateSet.contains(j)) {
-            duplicateSet.add(j)
-            j = 0
-            i++
-        } else {
-            // 우리가 찾아야 할거는 안되는 조건만 찾으면 된다.
-            // 끝까지 왔는데도 못찾았다면 false를 반환하면 된다. 필요없는 공간을 차지 할필요가 없다.
-            if (j == b.length - 1) {
-                return false
-            }
-            j++
-        }
+    for (char in a) {
+        mapOfAChar[char] = (mapOfAChar[char] ?: 0) + 1
     }
 
-    return true
+    for(char in b) {
+        mapOfBChar[char] = (mapOfBChar[char] ?: 0) + 1
+    }
+
+    // 시간 복잡도 O(N) 각 키를 조회하면서 키값을 비교해준다. 선형적으로 증가함.
+    return mapOfAChar == mapOfBChar
 }
